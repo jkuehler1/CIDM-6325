ae44728176d846f2aa6fa35b3df6b42fe572ace3
jkuehler1 <112365315+jkuehler1@users.noreply.github.com>
2023-11-30 02:39:08 -0600

    Add files via upload

---
+++ degree_checklist/degreechecklist01/degree_checklist/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Course, Student, Enrollment
+from .forms import UploadFileForm
+
+
+class CustomAdmin(admin.ModelAdmin):
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['upload_form'] = UploadFileForm()
+        return context
+
+
+class CourseAdmin(CustomAdmin):
+    change_form_template = 'admin/degree_checklist/course/change_form.html'
+
+
+class StudentAdmin(CustomAdmin):
+    change_form_template = 'admin/degree_checklist/student/change_form.html'
+
+
+class EnrollmentAdmin(CustomAdmin):
+    change_form_template = 'admin/degree_checklist/enrollment/change_form.html'
+
+
+if Course in admin.site._registry:
+    admin.site.unregister(Course)
+admin.site.register(Course, CourseAdmin)
+
+if Student in admin.site._registry:
+    admin.site.unregister(Student)
+admin.site.register(Student, StudentAdmin)
+
+if Enrollment in admin.site._registry:
+    admin.site.unregister(Enrollment)
+admin.site.register(Enrollment, EnrollmentAdmin)
